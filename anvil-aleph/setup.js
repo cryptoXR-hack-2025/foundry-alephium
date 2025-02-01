74418cb6e929514844e355968169942a6ac337bd
ismail.lefriyekh@mazars.fr <ismail.lefriyekh@mazars.fr>
2025-02-01 12:23:40 +0100

    [CHORE] group context for anvil

--- anvil-aleph/setup.js
+++ anvil-aleph/setup.js
@@ -1,27 +1,11 @@
-import { TransactionBuilder, DUST_AMOUNT } from '@alephium/web3'
+import { TransactionBuilder } from '@alephium/web3'
 import { HDWallet } from '@alephium/web3-wallet'
 import { NodeProvider, ExplorerProvider } from '@alephium/web3'
+import dotenv from 'dotenv';
 
+dotenv.config()
 
-const nodeUrl = 'http://127.0.0.1:22973';
-const explorerUrl = 'http://127.0.0.1:9090';
-const nodeProvider = new NodeProvider(nodeUrl);
-const explorerProvider = new ExplorerProvider(explorerUrl);
-const builder = TransactionBuilder.from(nodeUrl)
-
-const mnemonic_master = "vault alarm sad mass witness property virus style good flower rice alpha viable evidence run glare pretty scout evil judge enroll refuse another lava";
-const master_signer = new HDWallet({mnemonic:mnemonic_master, nodeProvider:nodeProvider, explorerProvider:explorerProvider});
-const account_master = await master_signer.deriveAndAddNewAccount(0);
-const master_balance = 1000000000000000000000000n;
-
-const master_data = {
-    mnemonic_master: mnemonic_master,
-    master_signer: master_signer,
-    master_address: account_master.address,
-    master_pubkey: account_master.publicKey
-}
-
-const init_accounts = async (nbAccounts) => {
+const init_accounts = async (nbAccounts, nodeProvider, explorerProvider) => {
     let accounts = []
     for (let idx=0; idx < nbAccounts; idx++) {
         const wallet_data = {
@@ -40,32 +24,57 @@ const init_accounts = async (nbAccounts) => {
     return accounts
 }
 
-const feed_accounts = async (accounts, master_data) => {
-    const amount = master_data.master_balance / accounts.length;
-    const senderAddress = account_master.address;
-    const senderPublicKey = account_master.publicKey;
-
+const feed_accounts = async (accounts, master_data, builder) => {
+    const amount = master_data.master_balance / BigInt(accounts.length + 1);
+    const senderAddress = master_data.master_address;
+    const senderPublicKey = master_data.master_pubkey;
     for (const account of accounts) {
         const receiverAddress = account.address;
-        const buildTxResult = await builder.buildTransferTx(
-          {
-            signerAddress: senderAddress,
-            destinations: [{
-              address: receiverAddress,
-              attoAlphAmount: DUST_AMOUNT,
-              tokens: [{
-                id: '19246e8c2899bc258a1156e08466e3cdd3323da756d8a543c7fc911847b96f00',
-                amount: amount
-              }]
-            }]
-          },
-          senderPublicKey
-        )
-        const result = await master_signer.signAndSubmitUnsignedTx({
-            signerAddress: account_master.address,
-            unsignedTx: buildTxResult.unsignedTx
-        })
+        try {
+            const buildTxResult = await builder.buildTransferTx(
+                {
+                  signerAddress: senderAddress,
+                  destinations: [{
+                    address: receiverAddress,
+                    attoAlphAmount: amount,
+                  }]
+                },
+                senderPublicKey
+            )
+            const result = await master_data.master_signer.signAndSubmitUnsignedTx({
+                signerAddress: account_master.address,
+                unsignedTx: buildTxResult.unsignedTx
+            })
+        } catch (error) {
+            console.error('Not enough fund to feed accounts. Please restart anvil.');
+            process.exit(1);
+        }        
     }
 }
 
-init_accounts(5)
+
+const init_context = async () => {
+    const nodeUrl = process.env.NODE_URL
+    const explorerUrl = process.env.EXPLORER_URL;
+    const nodeProvider = new NodeProvider(nodeUrl);
+    const explorerProvider = new ExplorerProvider(explorerUrl);
+    const builder = TransactionBuilder.from(nodeUrl);
+    
+    const mnemonic_master = process.env.MNEMONIC_MASTER;
+    const master_signer = new HDWallet({mnemonic:mnemonic_master, nodeProvider:nodeProvider, explorerProvider:explorerProvider});
+    const account_master = await master_signer.deriveAndAddNewAccount(0);
+    const master_data = {
+        mnemonic_master: mnemonic_master,
+        master_signer: master_signer,
+        master_address: account_master.address,
+        master_pubkey: account_master.publicKey,
+        master_balance: 1000000000000000000000000n
+    }
+    
+    const nbAccounts = 10;
+
+    const accounts = await init_accounts(10, nodeProvider, explorerProvider)
+    await feed_accounts(accounts, master_data, builder)
+}
+
+export default function init_context();
